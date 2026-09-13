faa0ef4becf45729a3cbb5d44ec0dcb656b77766
wso4133560 <wso4133560@gmail.com>
2026-09-13 14:56:08 +0800

    perf: avoid duplicate mappable blob flush

--- VirtualBox-7.2.6/src/VBox/Devices/VirtIO/DevVirtioGPU.cpp
+++ VirtualBox-7.2.6/src/VBox/Devices/VirtIO/DevVirtioGPU.cpp
@@ -1011,9 +1011,7 @@ static int virtioGpuR3VulkanResourceEnsureBuffer(PVIRTIOGPU pThis, PVIRTIOGPURES
     if (pRes->fSharedMemory && pRes->pvVkMapped && pRes->pbPixels)
     {
         memcpy(pRes->pvVkMapped, pRes->pbPixels, (size_t)pRes->cbPixels);
-        int const rcFlush = virtioGpuR3VulkanResourceMemoryOp(pThis, pRes, false);
-        if (RT_FAILURE(rcFlush))
-            return rcFlush;
+        return virtioGpuR3VulkanResourceMemoryOp(pThis, pRes, false);
     }
     return virtioGpuR3VulkanResourceMemoryOp(pThis, pRes, false);
 }
